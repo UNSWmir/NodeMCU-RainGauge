f4196ccfff84a5aae9889368c826d3ada134c070
Shane Cox <shane2@Salyut.local>
2018-04-29 21:24:21 +1000

    Arduino code upload

---
+++ rainGauge.ino
@@ -0,0 +1,63 @@
+#include <readiness_io.h>
+#include <Ticker.h>
+#include "config.h"
+
+
+const int       LED_PIN        = 0;     // The pin connecting the LED (D3)
+const int       INTERRUPT_PIN  = 14;    // The pin connect the test button (D5)
+volatile double RAIN_HEIGHT   = 0;     // variable for storing the rain_height
+
+volatile byte interrupt = 0;
+
+readiness_io client(CHANNEL_ID, TOPIC, SENSOR_ID, VERSION, FORMAT);
+Ticker timer;
+
+
+/* Interrupt for counting the number of clicks of the rain gauge */
+void rainInterrupt() {
+  RAIN_HEIGHT += BUCKETTIP_HEIGHT; // takes the current rain height and add the amount of the bucket
+  Serial.print("Curent Rain Height Collected (mm): ");
+  Serial.println(RAIN_HEIGHT);
+}
+
+void handleInterrupt() {
+  interrupt++;
+}
+
+/* Interrupt timer for sending data to the Readiness.io server */
+void writeToServer(){
+  interrupt++;
+}
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, HIGH); // internal LED is switched on when low - so we have to switch it off/
+
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
+  while(!Serial) { } // Wait for serial to initialize.
+  Serial.println("Device Started");
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  client.wifiConnection(WIFI_SSID, WIFI_PASS);
+
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP); // Set the interrupt pin for the reed/hall effect
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), rainInterrupt, RISING);  // Attach the interrupt.
+  timer.attach(UPDATE_RATE, writeToServer);
+
+  client.testConnection();
+}
+
+
+void loop() {
+  if(interrupt>0){
+    client.publishData(RAIN_HEIGHT);
+    RAIN_HEIGHT = 0;
+    interrupt=0;
+    digitalWrite(LED_PIN,HIGH);
+    delay(250);
+    digitalWrite(LED_PIN,LOW);
+
+  }
+}
